8539ffdc560ce3df44bee561ab2b7329f8496db5
Mario Visic <mario.visic@squaretalent.com>
2010-08-16 04:43:42 +0800

    fixed a bug which caused incompatability with newer paperclipped versions

--- app/helpers/admin/galleries_helper.rb
+++ app/helpers/admin/galleries_helper.rb
@@ -1,7 +1,7 @@
 module Admin::GalleriesHelper
   
   def gallery_available_assets(gallery)
-    @assets = Asset.search('', {:image => 1}) - gallery.assets
+    @assets = Asset.find_all_by_asset_content_type(Mime::IMAGE.all_types) - gallery.assets
     
     if false
       @result = []
